05ce8b4429229f8b5947896ba63d59ae804f54b2
asumanwebdev <asumanwebdev@gmail.com>
2022-06-17 01:38:31 +0300

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
-import shopifyConfig from './shopify.config'
+import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
+import shopifyConfig from './hydrogen.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
